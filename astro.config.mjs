c3e1b95cce42c5e92f5e9f8690125a2a837733ef
Jeroen <46943193+jeroenzeelmaekers@users.noreply.github.com>
2024-09-11 19:00:23 +0200

    feat(cms): add sanity integration (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,30 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  import.meta.env.MODE,
+  process.cwd(),
+  ''
+);
+
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
+const projectId = PUBLIC_SANITY_PROJECT_ID;
+const dataset = PUBLIC_SANITY_DATASET;
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: projectId,
+      dataset: dataset,
+      useCdn: false,
+      apiVersion: '2024-07-24',
+      studioBasePath: '/admin',
+    }),
+    react(),
+  ],
 });
